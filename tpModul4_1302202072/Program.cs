6ce9e957fb9d3569a2a2d809afb111dac1c5f5a1
razqyaqil <67795918+razqyaqil@users.noreply.github.com>
2022-03-18 15:33:44 +0700

    commit akhir
    benerin code yang keapus pas conflict :))

--- tpModul4_1302202072/Program.cs
+++ tpModul4_1302202072/Program.cs
@@ -7,7 +7,10 @@ namespace tpModul4_1302202072
     {
         public static void Main(string[] args)
         {
-
+            
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser<String>("razqy");
+            
             DataGeneric<String> NIM = new DataGeneric<string>("1302202072");
             NIM.printData();
             
